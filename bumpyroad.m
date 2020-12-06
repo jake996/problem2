c86d33daac9a7219a63adeaf5228e9ad75c00c83
Jake <jacoba1@kpit.com>
2020-12-06 13:32:51 +0530

    trying different methods to make bumpy road

--- bumpyroad.m
+++ bumpyroad.m
@@ -1,4 +1,12 @@
-th = linspace(0,3*pi) ;
-r=0.1*sin(th) ; 
-r(th>pi)=0 ;
-plot(th,r,'linewidth',10)
+%//th = linspace(0,3*pi) ;
+%r=0.1*sin(th) ; 
+%r(th>pi)=0 ;
+%plot(th,r,'linewidth',10)
+% L = Length,    A = Amplitude,
+sinbump = @(L,A) A*(0.5+0.5*(sin(linspace(-pi/2,pi*3/2,25*L))));
+
+B2 = sinbump(5, 0.5);
+plot(B2)
+grid
+title('Amplitude = 0.5.  Length = 125')
+axis([0  150    0  1.5])
